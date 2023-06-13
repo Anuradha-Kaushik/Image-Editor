982585957fbda628d483a7eeb674496e84720fa1
Anuradha-Kaushik <73479256+Anuradha-Kaushik@users.noreply.github.com>
2023-06-13 21:04:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
+from os import listdir
 import cv2
 import numpy as np
-from flask import Flask, flash, request, redirect, url_for, render_template,send_file
+from flask import Flask, flash, request, redirect, url_for, render_template,send_file, make_response
 from werkzeug.utils import secure_filename
+from io import BytesIO
+
 import warnings
 warnings.simplefilter("ignore", DeprecationWarning)
 
@@ -51,9 +54,9 @@ def upload_image():
 def make_grayscale(decode_array_to_img):
 
     converted_gray_img = cv2.cvtColor(decode_array_to_img, cv2.COLOR_RGB2GRAY)
-    status, output_image = cv2.imencode('.PNG', converted_gray_img)
+    status, output_img = cv2.imencode('.PNG', converted_gray_img)
 
-    return output_image
+    return output_img.tobytes()
 
 
 
@@ -65,50 +68,55 @@ def image_sketch(decode_array_to_img):
     sharping_blur_img = cv2.bitwise_not(blur_img)
     sketch_img = cv2.divide(converted_gray_img, sharping_blur_img, scale=256.0)
     status, output_img = cv2.imencode('.PNG', sketch_img)
-    return output_img
+    return output_img.tobytes()
 
 def image_oil(decode_array_to_img):
     oil_img = cv2.xphoto.oilPainting(decode_array_to_img,7,1)
     status, output_img = cv2.imencode('.PNG', oil_img)
-    return output_img
+    return output_img.tobytes()
 
 def image_rgba(decode_array_to_img):
     rgba_img = cv2.cvtColor(decode_array_to_img,cv2.COLOR_BGR2RGB)
     status, output_img = cv2.imencode('.PNG', rgba_img)
-    return output_img
+    return output_img.tobytes()
 
 def image_wc(decode_array_to_img):
     wc_img = cv2.stylization(decode_array_to_img,sigma_s=65,sigma_r=0.6)
     status, output_img = cv2.imencode('.PNG', wc_img)
-    return output_img
+    return output_img.tobytes()
 
 def image_invert(decode_array_to_img):
     inv_img = cv2.bitwise_not(decode_array_to_img)
     status, output_img = cv2.imencode('.PNG', inv_img)
-    return output_img
+    return output_img.tobytes()
 
 def image_hdr(decode_array_to_img):
     hdr_img = cv2.detailEnhance(decode_array_to_img,sigma_s=12,sigma_r=0.15)
     status, output_img = cv2.imencode('.PNG', hdr_img)
-    return output_img
+    return output_img.tobytes()
 
-@app.route("/download")
-def download_file():
-    with open('img.png','w') as i:
-        i.write(decode_array_to_img)
-    path = "static/img.png"
-    return send_file(path,as_attachment=True)
+@app.route("/download/<filename>")
+def download_file(filename):
+    file_path = os.path.join('static', filename)
+    response = make_response(send_file(file_path))
+    response.headers['Content-Disposition'] = 'attachment; filename=' + filename
+    return response
 
 @app.route('/display/<filename>')
 def display_image(filename):
 
     return redirect(url_for('static', filename=filename))
 
-
-
-
 if __name__ == "__main__":
     app.run()
+
+
+
+
+
+
+
+
 
 
 
